a6adc18012ab68ea274565352d4840f572013a36
Thamires Branquini <121324998+ranpoluv@users.noreply.github.com>
2024-03-31 20:49:00 -0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 def guess(x):
-	random_number = random.randit(1, x)
+	random_number = random.randint(1, x)
 	guess = 0
 	while guess != random_number:
 		guess = int(input(f'Guess a number between 1 and {x}: '))
